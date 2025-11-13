1078e3bd193e1f4031e6cccd9f1e1c81773f3464
wateralsie <minee1913@gmail.com>
2025-11-13 23:54:52 +0900

    feat: 프로젝트 세팅

---
+++ src/test/java/com/wateralsie/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wateralsie.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

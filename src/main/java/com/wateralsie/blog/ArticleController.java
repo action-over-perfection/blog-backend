4f03b2c081b4447318b1c7171a2377f3db48da6d
wateralsie <minee1913@gmail.com>
2025-11-18 19:34:19 +0900

    feat: 블로그 글 수정 api

--- src/main/java/com/wateralsie/blog/ArticleController.java
+++ src/main/java/com/wateralsie/blog/ArticleController.java
@@ -1,10 +1,12 @@
 package com.wateralsie.blog;
 
+import java.util.Objects;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,5 +34,18 @@ public class ArticleController {
         ArticleEntity newArticle = ArticleEntity.from(article);
         articleRepository.save(newArticle);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/{articleId}")
+    public ResponseEntity<String> patchArticle(@PathVariable Long articleId, @RequestBody Article article) {
+        ArticleEntity originalArticle = articleRepository.findById(articleId).orElseThrow();
+        if (!Objects.equals(originalArticle.getTitle(), article.title())) {
+            originalArticle.setTitle(article.title());
+        }
+        if (!Objects.equals(originalArticle.getContent(), article.content())) {
+            originalArticle.setContent(article.content());
+        }
+        articleRepository.save(originalArticle);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/com/wateralsie/blog/ArticleEntity.java
+++ src/main/java/com/wateralsie/blog/ArticleEntity.java
@@ -4,11 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.util.Objects;
 
 @Entity
 public class ArticleEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String content;
@@ -34,5 +35,13 @@ public class ArticleEntity {
 
     public String getContent() {
         return content;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
     }
 }

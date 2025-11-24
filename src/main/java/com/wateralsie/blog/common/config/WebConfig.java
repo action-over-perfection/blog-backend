f7f08604b1a1cb657d35774d67e759b666764b6f
wateralsie <minee1913@gmail.com>
2025-11-24 19:39:55 +0900

    chore: 배포 관련 개선

--- src/main/java/com/wateralsie/blog/common/config/WebConfig.java
+++ src/main/java/com/wateralsie/blog/common/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
+                .allowedOriginPatterns(
                         "http://localhost:3000",
                         "https://*.vercel.app"
                 )
